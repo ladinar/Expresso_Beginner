4a4e5d4252e4b447af0cabae15bd229d1f2911ab
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2017-12-13 10:57:26 +0700

    check decrement functionality

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -16,9 +16,17 @@
 
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * This test demos a user clicking the decrement button and verifying that it properly decrease
@@ -35,7 +43,19 @@ public class OrderActivityBasicTest {
     //          - Check that the initial quantity is zero
     //          - Click on the decrement button
     //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+    @Rule
+    public ActivityTestRule<OrderActivity> mActivityTestRule =
+            new ActivityTestRule<>(OrderActivity.class);
 
     public void clickDecrementButton_ChangesQuantityAndCost() {
+
+        // Check the initial quantity variable is zero
+        onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
+        // Click on decrement button
+        onView((withId(R.id.decrement_button))).perform(click());
+        // Verify that the decrement button decreases the quantity by 1
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+        // Verify that the increment button also increases the total cost to $5.00
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
     }
 }
